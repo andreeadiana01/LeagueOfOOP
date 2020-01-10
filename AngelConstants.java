9cfdc94b914719a71a703b11ffadb2faedc3f478
andreeadiana01 <48826378+andreeadiana01@users.noreply.github.com>
2020-01-10 14:59:07 +0200

    Add files via upload

---
+++ AngelConstants.java
@@ -0,0 +1,86 @@
+package constants;
+
+public final class AngelConstants {
+    private AngelConstants() { }
+
+//    DamageAngel
+
+    public static final float DMG_ANGEL_ON_KNIGHT = 0.15f;
+    public static final float DMG_ANGEL_ON_PYRO = 0.2f;
+    public static final float DMG_ANGEL_ON_ROGUE = 0.3f;
+    public static final float DMG_ANGEL_ON_WIZARD = 0.4f;
+
+//    Dark Angel
+
+    public static final int DARK_ANGEL_ON_KNIGHT = -40;
+    public static final int DARK_ANGEL_ON_PYRO = -30;
+    public static final int DARK_ANGEL_ON_ROGUE = -10;
+    public static final int DARK_ANGEL_ON_WIZARD = -20;
+
+//    Dracula
+
+    public static final float DRACULA_DMG_ON_KNIGHT = -0.2f;
+    public static final float DRACULA_DMG_ON_PYRO = -0.3f;
+    public static final float DRACULA_DMG_ON_ROGUE = -0.1f;
+    public static final float DRACULA_DMG_ON_WIZARD = -0.4f;
+
+    public static final int DRACULA_HP_ON_KNIGHT = -60;
+    public static final int DRACULA_HP_ON_PYRO = -40;
+    public static final int DRACULA_HP_ON_ROGUE = -35;
+    public static final int DRACULA_HP_ON_WIZARD = -40;
+
+
+//    GoodBoy
+
+    public static final float GB_DMG_ON_KNIGHT = 0.4f;
+    public static final float GB_DMG_ON_PYRO = 0.5f;
+    public static final float GB_DMG_ON_ROGUE = 0.4f;
+    public static final float GB_DMG_ON_WIZARD = 0.3f;
+
+    public static final int GB_HP_ON_KNIGHT = 20;
+    public static final int GB_HP_ON_PYRO = 30;
+    public static final int GB_HP_ON_ROGUE = 40;
+    public static final int GB_HP_ON_WIZARD = 50;
+
+//    LevelUpAngel
+
+    public static final float LVL_UP_ANGEL_ON_KNIGHT = 0.1f;
+    public static final float LVL_UP_ANGEL_ON_PYRO = 0.2f;
+    public static final float LVL_UP_ANGEL_ON_ROGUE = 0.15f;
+    public static final float LVL_UP_ANGEL_ON_WIZARD = 0.25f;
+
+//    Lifegiver
+
+    public static final int LIFEGIVER_ON_KNIGHT = 100;
+    public static final int LIFEGIVER_ON_PYRO = 80;
+    public static final int LIFEGIVER_ON_ROGUE = 90;
+    public static final int LIFEGIVER_ON_WIZARD = 120;
+
+//    SmallAngel
+
+    public static final float SMALL_DMG_ON_KNIGHT = 0.1f;
+    public static final float SMALL_DMG_ON_PYRO = 0.15f;
+    public static final float SMALL_DMG_ON_ROGUE = 0.05f;
+    public static final float SMALL_DMG_ON_WIZARD = 0.1f;
+
+    public static final int SMALL_HP_ON_KNIGHT = 10;
+    public static final int SMALL_HP_ON_PYRO = 15;
+    public static final int SMALL_HP_ON_ROGUE = 20;
+    public static final int SMALL_HP_ON_WIZARD = 25;
+
+//    Spawner
+
+    public static final int RESET_ON_KNIGHT = 200;
+    public static final int RESET_ON_PYRO = 150;
+    public static final int RESET_ON_ROGUE = 180;
+    public static final int RESET_ON_WIZARD = 120;
+
+
+//    XPAngel
+
+    public static final int XP_ANGEL_ON_KNIGHT = 45;
+    public static final int XP_ANGEL_ON_PYRO = 50;
+    public static final int XP_ANGEL_ON_ROGUE = 40;
+    public static final int XP_ANGEL_ON_WIZARD = 60;
+
+}
